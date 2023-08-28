69e420bf3e1a03594a6d8a3cdc5288e31814555d
Nursultan <nursultanyelemessov@gmail.com>
2023-08-28 14:32:22 +0600

    Initial Commit

---
+++ SWapiMontekrist/SWapiMontekristApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWapiMontekristApp.swift
+//  SWapiMontekrist
+//
+//  Created by Nursultan Yelemessov on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SWapiMontekristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
